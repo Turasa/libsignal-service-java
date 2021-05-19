b4a977c972db09ce73c9077d1568f69acbd937e1
AsamK <asamk@gmx.de>
2021-05-19 22:00:40 +0200

    Implement support for uploading stickers
    Originally implemented by Signal Stickers

--- service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -42,6 +42,7 @@ import org.whispersystems.signalservice.api.account.ChangePhoneNumberRequest;
 import org.whispersystems.signalservice.api.account.PniKeyDistributionRequest;
 import org.whispersystems.signalservice.api.account.PreKeyCollection;
 import org.whispersystems.signalservice.api.account.PreKeyUpload;
+import org.whispersystems.signalservice.api.crypto.AttachmentCipherStreamUtil;
 import org.whispersystems.signalservice.api.crypto.UnidentifiedAccess;
 import org.whispersystems.signalservice.api.groupsv2.CredentialResponse;
 import org.whispersystems.signalservice.api.groupsv2.GroupsV2AuthorizationString;
@@ -130,6 +131,7 @@ import org.whispersystems.signalservice.internal.push.http.DigestingRequestBody;
 import org.whispersystems.signalservice.internal.push.http.NoCipherOutputStreamFactory;
 import org.whispersystems.signalservice.internal.push.http.OutputStreamFactory;
 import org.whispersystems.signalservice.internal.push.http.ResumableUploadSpec;
+import org.whispersystems.signalservice.internal.push.http.StickerCipherOutputStreamFactory;
 import org.whispersystems.signalservice.internal.storage.protos.ReadOperation;
 import org.whispersystems.signalservice.internal.storage.protos.StorageItems;
 import org.whispersystems.signalservice.internal.storage.protos.StorageManifest;
@@ -240,6 +242,7 @@ public class PushServiceSocket {
   private static final String UUID_ACK_MESSAGE_PATH     = "/v1/messages/uuid/%s";
   private static final String ATTACHMENT_V2_PATH        = "/v2/attachments/form/upload";
   private static final String ATTACHMENT_V4_PATH        = "/v4/attachments/form/upload";
+  private static final String STICKER_UPLOAD_PATH       = "/v1/sticker/pack/form/%d";
 
   private static final String PAYMENTS_AUTH_PATH        = "/v1/payments/auth";
 
@@ -1393,6 +1396,16 @@ public class PushServiceSocket {
     }
   }
 
+  public StickerUploadAttributesResponse getStickerUploadAttributes(int stickerCount) throws NonSuccessfulResponseCodeException, PushNetworkException, MalformedResponseException {
+    String response = makeServiceRequest(String.format(Locale.ROOT, STICKER_UPLOAD_PATH, stickerCount), "GET", null);
+    try {
+      return JsonUtil.fromJson(response, StickerUploadAttributesResponse.class);
+    } catch (IOException e) {
+      Log.w(TAG, e);
+      throw new MalformedResponseException("Unable to parse entity", e);
+    }
+  }
+
   public AttachmentDigest uploadGroupV2Avatar(byte[] avatarCipherText, AvatarUploadAttributes uploadAttributes)
       throws IOException
   {
@@ -1455,6 +1468,25 @@ public class PushServiceSocket {
                           attachment.getCancelationSignal(),
                           attachment.getResumableUploadSpec().getHeaders());
     }
+  }
+
+  public void uploadStickerContent(InputStream content,long length, byte[] expandedPackKey, StickerUploadAttributes uploadAttributes)
+      throws NonSuccessfulResponseCodeException, PushNetworkException
+  {
+    uploadToCdn0("",
+                 uploadAttributes.getAcl(),
+                 uploadAttributes.getKey(),
+                 uploadAttributes.getPolicy(),
+                 uploadAttributes.getAlgorithm(),
+                 uploadAttributes.getCredential(),
+                 uploadAttributes.getDate(),
+                 uploadAttributes.getSignature(),
+                 content,
+                 "application/octet-stream",
+                 AttachmentCipherStreamUtil.getCiphertextLength(length),
+                 new StickerCipherOutputStreamFactory(expandedPackKey),
+                 null,
+                 null);
   }
 
   private void downloadFromCdn(File destination, int cdnNumber, String path, long maxSizeBytes, ProgressListener listener)
