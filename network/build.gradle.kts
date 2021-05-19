aa9f9e5eb3be02255fd6cd81f48147114cb7d8d5
AsamK <asamk@gmx.de>
2021-05-19 20:42:11 +0200

    Adapt build files for deployment of the fork

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -10,10 +10,14 @@ plugins {
   id("java-library")
   id("org.jetbrains.kotlin.jvm")
   id("idea")
+  id("maven-publish")
+  id("signing")
   alias(libs.plugins.ktlint)
 }
 
 java {
+  withJavadocJar()
+  withSourcesJar()
   sourceCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
   targetCompatibility = JavaVersion.toVersion(libs.versions.javaVersion.get())
 }
@@ -58,4 +62,46 @@ dependencies {
   testImplementation(testLibs.junit.junit)
   testImplementation(testLibs.assertk)
   testImplementation(testLibs.mockk)
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+      from(components["java"])
+      artifactId = "signal-network"
+
+      pom {
+        name.set("signal-network")
+        description.set("Signal Service communication library for Java, unofficial fork")
+        url.set("https://github.com/Turasa/libsignal-service-java")
+        licenses {
+          license {
+            name.set("GPLv3")
+            url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+          }
+        }
+        developers {
+          developer {
+            name.set("Moxie Marlinspike")
+          }
+          developer {
+            name.set("Sebastian Scheibner")
+          }
+          developer {
+            name.set("Tilman Hoffbauer")
+          }
+        }
+        scm {
+          connection.set("scm:git@github.com:Turasa/libsignal-service-java.git")
+          developerConnection.set("scm:git@github.com:Turasa/libsignal-service-java.git")
+          url.set("scm:git@github.com:Turasa/libsignal-service-java.git")
+        }
+      }
+    }
+  }
+}
+
+signing {
+  isRequired = gradle.taskGraph.hasTask("uploadArchives")
+  sign(publishing.publications["mavenJava"])
 }
