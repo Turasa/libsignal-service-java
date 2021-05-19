e1c3be44cdec0a279ac68e2a87c7fda8765e4e0d
AsamK <asamk@gmx.de>
2021-05-19 21:06:43 +0200

    Enable clean shutdown of network threads
    - Stop okHttp executorService after websocket connection is closed
      Otherwise the ThreadPoolExecutor thread will keep running for 60 seconds

--- service/src/main/java/org/whispersystems/signalservice/internal/websocket/OkHttpWebSocketConnection.java
+++ service/src/main/java/org/whispersystems/signalservice/internal/websocket/OkHttpWebSocketConnection.java
@@ -75,6 +75,7 @@ public class OkHttpWebSocketConnection extends WebSocketListener implements WebS
   private final String                                    extraPathUri;
   private final SecureRandom                              random;
 
+  private OkHttpClient okHttpClient;
   private WebSocket client;
 
   public OkHttpWebSocketConnection(String name,
@@ -147,7 +148,7 @@ public class OkHttpWebSocketConnection extends WebSocketListener implements WebS
         clientBuilder.socketFactory(new TlsProxySocketFactory(signalProxy.get().getHost(), signalProxy.get().getPort(), dns));
       }
 
-      OkHttpClient okHttpClient = clientBuilder.build();
+      okHttpClient = clientBuilder.build();
 
       Request.Builder requestBuilder = new Request.Builder().url(wsUri);
 
@@ -190,6 +191,7 @@ public class OkHttpWebSocketConnection extends WebSocketListener implements WebS
       client.close(1000, "OK");
       client = null;
       webSocketState.onNext(WebSocketConnectionState.DISCONNECTING);
+      okHttpClient.dispatcher().executorService().shutdown();
     }
 
     notifyAll();
@@ -345,6 +347,11 @@ public class OkHttpWebSocketConnection extends WebSocketListener implements WebS
     }
 
     cleanupAfterShutdown();
+
+    if (okHttpClient != null ) {
+      okHttpClient.dispatcher().executorService().shutdown();
+      okHttpClient = null;
+    }
 
     notifyAll();
   }
