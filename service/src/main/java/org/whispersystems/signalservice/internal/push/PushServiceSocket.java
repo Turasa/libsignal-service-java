bfa75d2e20f920a2ebe7762c40f8adfa9819cc39
AsamK <asamk@gmx.de>
2021-05-19 21:06:43 +0200

    Enable clean shutdown of network threads
    - Stop okHttp executorService after websocket connection is closed
      Otherwise the ThreadPoolExecutor thread will keep running for 60 seconds

--- service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -147,6 +147,7 @@ import org.signal.core.util.Base64;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -201,7 +202,7 @@ import okhttp3.ResponseBody;
 /**
  * @author Moxie Marlinspike
  */
-public class PushServiceSocket {
+public class PushServiceSocket implements Closeable {
 
   private static final String TAG = PushServiceSocket.class.getSimpleName();
 
@@ -2403,6 +2404,21 @@ public class PushServiceSocket {
       throws PushNetworkException, MalformedResponseException
   {
       return readBodyJson(response.body(), clazz);
+  }
+
+  @Override
+  public void close() {
+    for (var holder : serviceClients) {
+      holder.getClient().dispatcher().executorService().shutdown();
+    }
+    for (var holders : cdnClientsMap.values()) {
+      for (var holder : holders) {
+        holder.getClient().dispatcher().executorService().shutdown();
+      }
+    }
+    for (var holder : storageClients) {
+      holder.getClient().dispatcher().executorService().shutdown();
+    }
   }
 
   public enum VerificationCodeTransport { SMS, VOICE }
