6f0144b4079089dc2fce432c9ee8d8cfd9e2b12a
AsamK <asamk@gmx.de>
2021-05-19 21:06:43 +0200

    Enable clean shutdown of network threads
    - Stop okHttp executorService after websocket connection is closed
      Otherwise the ThreadPoolExecutor thread will keep running for 60 seconds

--- service/src/main/java/org/whispersystems/signalservice/internal/websocket/WebSocketConnection.java
+++ service/src/main/java/org/whispersystems/signalservice/internal/websocket/WebSocketConnection.java
@@ -72,6 +72,7 @@ public class WebSocketConnection extends WebSocketListener {
   private final boolean                                   allowStories;
   private final SignalServiceUrl                          serviceUrl;
 
+  private OkHttpClient okHttpClient;
   private WebSocket client;
 
   public WebSocketConnection(String name,
@@ -149,7 +150,7 @@ public class WebSocketConnection extends WebSocketListener {
         clientBuilder.socketFactory(new TlsProxySocketFactory(signalProxy.get().getHost(), signalProxy.get().getPort(), dns));
       }
 
-      OkHttpClient okHttpClient = clientBuilder.build();
+      okHttpClient = clientBuilder.build();
 
       Request.Builder requestBuilder = new Request.Builder().url(filledUri);
 
@@ -332,6 +333,11 @@ public class WebSocketConnection extends WebSocketListener {
     }
 
     cleanupAfterShutdown();
+
+    if (okHttpClient != null ) {
+      okHttpClient.dispatcher().executorService().shutdown();
+      okHttpClient = null;
+    }
 
     notifyAll();
   }
