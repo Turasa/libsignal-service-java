7f420c977b1cd4316925cdaca47214230ee4a130
AsamK <asamk@gmx.de>
2021-05-19 21:57:15 +0200

    Implement device provisioning support as linked device
    Originally implmeneted by Turakar

--- service/src/main/java/org/whispersystems/signalservice/api/messages/SignalServiceContent.java
+++ service/src/main/java/org/whispersystems/signalservice/api/messages/SignalServiceContent.java
@@ -884,6 +884,10 @@ public final class SignalServiceContent {
       return SignalServiceSyncMessage.forMessageRequestResponse(responseMessage);
     }
 
+    if (content.hasGroups()) {
+      return SignalServiceSyncMessage.forGroups(createAttachmentPointer(content.getGroups().getBlob()));
+    }
+
     if (content.hasOutgoingPayment()) {
       SignalServiceProtos.SyncMessage.OutgoingPayment outgoingPayment = content.getOutgoingPayment();
       switch (outgoingPayment.getPaymentDetailCase()) {
@@ -918,6 +922,10 @@ public final class SignalServiceContent {
 
     if (content.hasContacts()) {
       return SignalServiceSyncMessage.forContacts(new ContactsMessage(createAttachmentPointer(content.getContacts().getBlob()), content.getContacts().getComplete()));
+    }
+
+    if (content.hasPniIdentity()) {
+      return SignalServiceSyncMessage.forPniIdentity(content.getPniIdentity());
     }
 
     return SignalServiceSyncMessage.empty();
