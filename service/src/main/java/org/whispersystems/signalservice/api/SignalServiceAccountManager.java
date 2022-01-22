c938083ca74218cedc9a5e4c1d7714a9b7e7b1d7
AsamK <asamk@gmx.de>
2022-01-22 16:35:52 +0100

    Make deviceId an int everywhere

--- service/src/main/java/org/whispersystems/signalservice/api/SignalServiceAccountManager.java
+++ service/src/main/java/org/whispersystems/signalservice/api/SignalServiceAccountManager.java
@@ -829,7 +829,7 @@ public class SignalServiceAccountManager {
     return this.pushServiceSocket.getDevices();
   }
 
-  public void removeDevice(long deviceId) throws IOException {
+  public void removeDevice(int deviceId) throws IOException {
     this.pushServiceSocket.removeDevice(deviceId);
   }
 
