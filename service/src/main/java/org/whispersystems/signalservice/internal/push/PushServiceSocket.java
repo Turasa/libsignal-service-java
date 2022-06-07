d270e685dfefd6bbd2ee809db0f19dea6549fe11
AsamK <asamk@gmx.de>
2022-06-07 13:23:02 +0200

    Log response body of bad request response

--- service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -2522,6 +2522,7 @@ public class PushServiceSocket {
             throw new PushNetworkException(e);
           }
 
+          Log.i(TAG, "Received 400 response, with body: " + body);
           if (body.isEmpty()) {
             throw new ImpossiblePhoneNumberException();
           } else {
