03f3bd6002d6f72e7ad1bd07fd91f362cc47a3bc
AsamK <asamk@gmx.de>
2022-06-11 09:33:34 +0200

    Improve parsing of BadRequest exception

--- service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -2441,7 +2441,10 @@ public class PushServiceSocket {
     if (body == null) {
       throw new MalformedResponseException("No body!");
     }
+    return readBodyStringNonNull(body);
+  }
 
+  private static String readBodyStringNonNull(ResponseBody body) throws PushNetworkException {
     try {
       return body.string();
     } catch (IOException e) {
