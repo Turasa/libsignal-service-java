f8bf4b05b52024e9b04cb4b3b59c7cb5e51a44c5
AsamK <asamk@gmx.de>
2022-10-06 17:27:22 +0200

    Bump version

--- constants.gradle.kts
+++ constants.gradle.kts
@@ -1,2 +1,2 @@
-val lib_signal_service_version_number by extra { "2.15.3" }
+val lib_signal_service_version_number by extra { "2.15.3_unofficial_139" }
 val lib_signal_service_group_info by extra { "com.github.turasa" }
