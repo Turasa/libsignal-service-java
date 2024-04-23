e9e92e719fb0f991d2b00bdeb2d39edfbead1c05
moiseev-signal <122060238+moiseev-signal@users.noreply.github.com>
2024-04-23 09:20:27 -0700

    [Signal-Android] Upgrade to libsignal 0.45.0 9b5387e221fc623c4453b163a6ef34cd70eb25d6

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,7 +1,7 @@
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
-      version("libsignal-client", "0.44.0")
+      version("libsignal-client", "0.45.1")
 
       // Kotlin
       version("kotlin", "1.8.10")
