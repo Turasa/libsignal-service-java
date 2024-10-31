3d8c735a2d6d249759a310f045f620298642de28
Greyson Parrelli <greyson@signal.org>
2024-10-31 11:58:40 -0400

    [Signal-Android] Move to separate message and media backup keys. 26b9cea88ef456d765c9b952f7f0fa76b57d6e4a

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,7 +1,7 @@
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
-      version("libsignal-client", "0.58.2")
+      version("libsignal-client", "0.60.1")
 
       // Kotlin
       version("kotlin", "1.9.20")
